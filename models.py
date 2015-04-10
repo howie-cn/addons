02f04f1a19e721c17ffabccf9f8fdac05be9b1e1
Jeffery Chen Fan <jeffery9@gmail.com>
2015-04-10 08:19:36 +0800

    first commit.

---
+++ models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class auth_oauth_extended(models.Model):
+#     _name = 'auth_oauth_extended.auth_oauth_extended'
+
+#     name = fields.Char()
